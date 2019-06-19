59998ecd4ea68e693d2409d4f8c57ee0bff72793
Jakub <qbazilla@hotmail.com>
2019-06-19 21:00:23 +0200

    changed snake structure to children list, rotating

--- snake.js
+++ snake.js
@@ -3,31 +3,24 @@ class Snake {
     elemSize = 1.16;
 
     constructor(length, color1 = 0xFFFFFF, color2 = 0x0000FF) {
-        this.array = this.generateArray(length, color1, color2);
-        this.vectors = [];
-        for(var i = 0; i < this.array.length; i++) {
-            if (i % 2 == 0) {
-                this.vectors.push(new THREE.Vector3(0, 1, 0));
-            } else {
-                this.vectors.push(new THREE.Vector3(0, 1 / Math.sqrt(2), 1 / Math.sqrt(2)));
-            }
-        }
+        this.head = this.generateSnake(length, color1, color2);
+        this.length = length;
     }
 
-    generateArray(length, color1 = 0xFFFFFF, color2 = 0x0000FF) {
-        var array = [];
-        var yy = 0;
-        for (var i = 0; i < length; i++) {
-            var mesh = i % 2 === 0 ?
+    generateSnake(length, color1 = 0xFFFFFF, color2 = 0x0000FF) {
+        var head = this.generateEvenElement(color1);
+        var p = head;
+        for (var i = 1; i < length; i++) {
+            var mesh = i % 2 == 0 ?
                 this.generateEvenElement(color1)
                 : this.generateOddElement(color2);
-            if (i % 2 === 0) { 
-                yy += this.elemSize;
+            if (i % 2 == 0) { 
+                mesh.position.y += this.elemSize;
             }
-            mesh.position.y += yy;
-            array.push(mesh);
+            p.add(mesh);
+            p = mesh;
         }
-        return array;
+        return head;
     }
 
     generateEvenElement(meshcolor) {
@@ -65,27 +58,17 @@ class Snake {
     }
 
     addToScene(scene) {
-        this.array.forEach((el) => scene.add(el));
+        scene.add(this.head);
     }
 
     rotate(index, angle) {
-        if(index > 0 && index < this.array.length) {
-            var dd = 0;
-            for(var i = index; i < this.array.length; i++) {
-                var el = this.array[i];
-                var v = new THREE.Vector3(0, 1 / Math.sqrt(2), 1 / Math.sqrt(2));
-                var v1 = new THREE.Vector3(0, 1, 0);
-
-                el.translateOnAxis(v1, -dd);
-
-                el.rotateOnAxis(this.vectors[index], angle);
-
-                el.translateOnAxis(v1, dd);
-
-                if (i % 2 == 1) {
-                    dd += this.elemSize;
-                }
+        if(index > 0 && index < this.length) {
+            var el = this.head;
+            for(var i = 0; i < index; i++) {
+                el = el.children[0];
             }
+            var v = index % 2 == 0 ? new THREE.Vector3(0, 1, 0) : new THREE.Vector3(0, 1 / Math.sqrt(2), 1 / Math.sqrt(2));
+            el.rotateOnAxis(v, angle);
         }
     }
 }
