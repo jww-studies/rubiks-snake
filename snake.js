992e8739038509748714240525f2ac8661cc4453
Jakub <qbazilla@hotmail.com>
2019-06-23 18:42:42 +0200

    Manually calculated position of last elem

--- snake.js
+++ snake.js
@@ -4,24 +4,29 @@ class Snake {
     selectionColor =  0x00FF00;
 
     constructor(length, color1 = 0xFFFFFF, color2 = 0x0000FF) {
-        this.head = this.generateSnake(length, color1, color2);
+        const tuple = this.generateSnake(length, color1, color2);
+        this.head = tuple.head;
+        this.tail = tuple.tail;
         this.length = length;
+        this.angles = new Array(this.length).fill(0);
     }
 
     generateSnake(length, color1 = 0xFFFFFF, color2 = 0x0000FF) {
         var head = this.generateEvenElement(color1);
         var p = head;
+        var tail = null;
         for (var i = 1; i < length; i++) {
             var mesh = i % 2 == 0 ?
                 this.generateEvenElement(color1)
                 : this.generateOddElement(color2);
                 mesh.position.x += this.elemSize * 2;
-            if (i % 2 == 0) { 
-            }
             p.add(mesh);
             p = mesh;
+            if(i === length - 1) {
+                tail = mesh;
+            }
         }
-        return head;
+        return {head: head, tail: tail};
     }
 
     generateEvenElement(meshcolor) {
@@ -84,7 +89,26 @@ class Snake {
             const direction = i % 2 == 0 ? -1 : 1;
             var v = new THREE.Vector3(direction / Math.sqrt(2), 1 / Math.sqrt(2), 0);
             el.rotateOnAxis(v, angle);
+            el.updateMatrixWorld(true);
         }
+    }
+
+    setAngle(i, angle) {
+        const index = parseInt(i, 0);
+        if(index > 0 && index < this.length) {
+            if(this.angles[index] !== angle) {
+                this.rotate(i, (angle - this.angles[index]) * Math.PI / 180);
+                this.angles[index] = angle;
+            }
+        }
+    }
+
+    getAngle(i) {
+        const index = parseInt(i, 0);
+        if(index > 0 && index < this.length) {
+            return this.angles[index];
+        }
+        return 0;
     }
 
     select(i) {
@@ -102,4 +126,38 @@ class Snake {
             el.material.color.setHex(this.selectionColor);
         }
     }
+
+    getEvenRotationMatrix(alpha) {
+        return math.matrix([[0.5 * (1 + Math.cos(alpha)), -0.5 * (1 - Math.cos(alpha)), 1 / Math.sqrt(2) * Math.sin(alpha), 0],
+                            [-0.5 * (1 - Math.cos(alpha)), 0.5 * (1 + Math.cos(alpha)), 1 / Math.sqrt(2) * Math.sin(alpha), 0],
+                            [-1 / Math.sqrt(2) * Math.sin(alpha), -1 / Math.sqrt(2) * Math.sin(alpha), Math.cos(alpha), 0],
+                            [0, 0, 0, 1]]);
+    }
+
+    getOddRotationMatrix(alpha) {
+        return math.matrix([[0.5 * (1 + Math.cos(alpha)), 0.5 * (1 - Math.cos(alpha)), 1 / Math.sqrt(2) * Math.sin(alpha), 0],
+                            [0.5 * (1 - Math.cos(alpha)), 0.5 * (1 + Math.cos(alpha)), -1 / Math.sqrt(2) * Math.sin(alpha), 0],
+                            [-1 / Math.sqrt(2) * Math.sin(alpha), 1 / Math.sqrt(2) * Math.sin(alpha), Math.cos(alpha), 0],
+                            [0, 0, 0, 1]]);
+    }
+
+    getLastElementPosition(manuallyCalculated = false) {
+        if(manuallyCalculated) {
+            var resultMatrix = math.identity(4);
+            for (var i = 1; i < this.length; ++i) {
+                const ang = this.angles[i] * Math.PI / 180;
+                const rotationMatrix = i % 2 == 0 ? this.getEvenRotationMatrix(ang) : this.getOddRotationMatrix(ang);
+                const translationMatrix1 = math.subset(math.identity(4), math.index(0, 3), -2 * i);
+                const translationMatrix2 = math.subset(math.identity(4), math.index(0, 3), 2 * i);
+                const tmp = math.multiply(translationMatrix2, math.multiply(rotationMatrix, translationMatrix1));
+                resultMatrix = math.multiply(resultMatrix, tmp);
+            }
+            const lastElement = math.multiply(resultMatrix, math.matrix([[2 * (this.length - 1)], [0], [0], [1]]));
+            return new THREE.Vector3(math.subset(lastElement, math.index(0,0)), math.subset(lastElement, math.index(1,0)), math.subset(lastElement, math.index(2,0)));
+        } else {
+            return this.tail.localToWorld(new THREE.Vector3(0,0,0));
+        }
+    }
+
+    
 }
